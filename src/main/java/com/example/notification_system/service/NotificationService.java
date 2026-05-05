0ba3c79b0223c4f6878ab6bce3253f0406a9d0af
Agustin Portillo <agustingp@kubofinanciero.com>
2026-05-05 15:27:27 -0600

    Step 3: Upgrade Maven Wrapper - Compile: SUCCESS

--- src/main/java/com/example/notification_system/service/NotificationService.java
+++ src/main/java/com/example/notification_system/service/NotificationService.java
@@ -2,16 +2,9 @@ package com.example.notification_system.service;
 
 import java.util.List;
 
-import org.springframework.stereotype.Service;
-
 import com.example.notification_system.model.Notification;
 
-@Service
-public class NotificationService {
+public interface NotificationService {
 
-    public void sendAll(List<Notification> notifications) {
-        for (Notification n : notifications) {
-            System.out.println(n.send());
-        }
-    }
+    List<String> sendAll(List<Notification> notifications);
 }
